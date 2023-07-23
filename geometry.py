eab3c9f0e899bc64ac2c2885d0a305adda0d2d3a
sabbir <sabbirahmed.engr@gmail.com>
2023-07-23 20:49:04 +0600

    File Upload

---
+++ geometry.py
@@ -0,0 +1,13 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return '({}, {})'.format(self.x, self.y)
+
+    def distance(self, anotherPoint):
+        if isinstance(anotherPoint, Point):
+            return round(pow((self.x - anotherPoint.x) ** 2 + (self.y - anotherPoint.y) ** 2, 1/2), 5)
+        else:
+            raise print('Distance can only be calculated between two points!')

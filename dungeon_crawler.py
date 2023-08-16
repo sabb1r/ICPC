892a3cd20a657511d0b58a46429d5d55d422910c
sabbir <sabbirahmed.engr@gmail.com>
2023-08-16 20:30:23 +0600

    Update

--- dungeon_crawler.py
+++ dungeon_crawler.py
@@ -5,32 +5,52 @@ import matplotlib.pyplot as plt
 class Room:
     def __init__(self, room_number):
         self.room_number = room_number
-        self.parent = set()
-        self.child = set()
+        self.edge = set()
         self.time = {}
 
-    def add_child(self, room, time):
-        self.child.add(room)
-        room.parent.add(self)
+    def add_link(self, room, time):
+        self.edge.add(room)
+        room.edge.add(self)
         self.time[(self.room_number, room.room_number)] = time
+        room.time[room.room_number, self.room_number] = time
 
 
 def time_counter(room):
-    if len(room.child) == 0:
+    if len(room.edge) == 0:
         return 0
     else:
         time = 0
-        for ch in room.child:
-            time += room.time[(room.room_number, ch.room_number)] + time_counter(ch)
+        for e in room.edge:
+            time += room.time[(room.room_number, e.room_number)] + time_counter(e)
     return time
+
+
+def show_path(room_from, room_to, room_throgh=None):
+    """
+    :param room_from: A room object
+    :param room_to: A room object
+    :param room_throgh: A room object
+    :return: True - if I have to face "room_through" while going from "room_from" to "room_to"
+    """
+
+    if len(room_from.edge) == 0:
+        return []
+    else:
+        path = []
+        for e in room_from.edge:
+            path.append(e)
+            if e.room_number == room_to.room_number:
+                return path
+            else:
+                path.append(show_path(e, room_to, room_throgh=None))
 
 
 def plot_tree(room):
     global G
     G.add_node(room.room_number)
-    for ch in room.child:
-        G.add_edge(room.room_number, ch.room_number)
-        plot_tree(ch)
+    for e in room.edge:
+        G.add_edge(room.room_number, e.room_number)
+        plot_tree(e)
 
 
 room1 = Room(1)
@@ -40,12 +60,12 @@ room4 = Room(4)
 room5 = Room(5)
 G = nx.Graph()
 
-room1.add_child(room2, 5)
-room1.add_child(room3, 1)
-room3.add_child(room4, 3)
-room3.add_child(room5, 2)
+room1.add_link(room2, 5)
+room1.add_link(room3, 1)
+room3.add_link(room4, 3)
+room3.add_link(room5, 2)
 
-print(time_counter(room3))
+
 plot_tree(room1)
 nx.draw(G, with_labels=True, font_weight='bold')
 plt.show()
